a3f1faa606f20c5b066e8cd43a59cb179571de9f
taymanov-bn <btaymanov07@gmail.com>
2026-04-23 16:10:30 +0300

    add heap module without function implementation

---
+++ include/heap/heap.h
@@ -0,0 +1,24 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include "tree/tree.h"
+
+
+typedef struct {
+  Node** heap;
+  int size;
+} Heap;
+
+Heap* create_heap();
+void add_heap(Heap* arr, Node* node);
+Node* get_min_heap(Heap* arr);
+void free_heap(Heap* arr);
+
+
+
+
+
+
+
+#endif
+
