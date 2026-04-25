1a72e694207c6107fffe93fd447726ddbee5d6d6
taymanov-bn <btaymanov07@gmail.com>
2026-04-25 20:06:55 +0300

    add function 'get_min_heap'

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -44,6 +44,52 @@ void add_heap(Heap* heap, Node* node) {
 }
 
 
+Node* get_min_heap(Heap* heap) {
+  Node* min_node = heap->arr[0];
+  heap->arr[0] = heap->arr[heap->size-1];
+  int index = 0;
+  int current_idx = 0;
+
+  while (index < heap->size) {
+    if ((index*2 + 1) >= heap->size && (index*2 + 2) >= heap->size) break;
+
+    if ((index*2 + 1) < heap->size && (index*2 + 2) >= heap->size) {
+      Node* min_both = heap->arr[index*2 + 1];
+      index = index*2 + 1;
+    }
+
+    if ((index*2 + 1) >= heap->size && (index*2 + 2) < heap->size) {
+      Node* min_both = heap->arr[index*2 + 2];
+      index = index*2 + 2;
+    }
+
+    if ((index*2 + 1) >= size && (index*2 + 2) >= size) {
+      heap->size--;
+      return min_node;
+    }
+
+    if (heap->arr[index] <= (heap->arr[index*2 + 1]->frequence + heap->arr[index*2 + 2]->frequence))
+      break;
+
+
+    if (heap->arr[index*2 + 1]->frequence > heap->arr[index*2 + 2]->frequence) {
+      Node* min_both = heap->arr[index*2 + 2];
+      index = index * 2 + 2;
+    } else {
+      Node* min_both = heap->arr[index*2 + 1];
+      index = index * 2 + 1;
+    }
+    if (heap->arr[i]->frequence > min_both->frequence) {
+      Node* temp = heap->arr[current_idx];
+      heap->arr[current_idx] = heap->arr[index];
+      heap->arr[index] = temp;
+      current_idx = index;
+    }
+  }
+  heap->size--;
+  return min_node;
+}
+
 
 void free_heap(Heap* heap) {
   if (!heap) {
