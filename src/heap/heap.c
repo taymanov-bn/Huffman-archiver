3d6b8bc65e845e2e471bc42352390a6190f7df50
taymanov-bn <btaymanov07@gmail.com>
2026-04-25 18:38:35 +0300

    add some functions

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -2,4 +2,54 @@
 #include "tree/tree.h"
 #include "heap/heap.h"
 
+#define HEAP_CAPACITY 256
 
+
+Heap* create_heap() {
+  Heap* heap;
+  heap = malloc(sizeof(Heap));
+  if (!heap) {
+    return NULL;
+  }
+  heap->arr = malloc(sizeof(Node*)*HEAP_CAPACITY);
+  if (!heap->arr) {
+    free(heap);
+    return NULL;
+  }
+  heap->size = 0;
+  return heap;
+}
+
+void add_heap(Heap* heap, Node* node) {
+  if (heap->size >= HEAP_CAPACITYP) 
+    return;
+  heap->arr[heap->size] = node;
+  if (heap->size > 0) {  
+    int index = heap->size;
+
+    while (index > 0) {
+      if (((index - 1) / 2) >= 0) {
+        if (heap->arr[index]->frequence < heap->arr[(index - 1) / 2)]->frequence) {
+          Node* temp = heap->arr[index]
+          heap->arr[index] = heap->arr[(index - 1) / 2];
+          index = (index - 1) / 2;
+          heap->arr[index] = temp;
+        }      
+      } else break;
+    }  
+
+  }
+  heap->size++;
+
+}
+
+
+
+void free_heap(Heap* heap) {
+  if (!heap) {
+    return;
+  }
+
+  free(heap->arr);
+  free(heap);
+}
